76b529bcf70dd7aca3b84936d3953628558155d6
Andreas Memmel <andreasmemmel@mailbox.org>
2022-04-02 18:21:28 +0200

    fixed link to picture

--- raw/main/userscript.user.js
+++ raw/main/userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/AndMSWDE97/place_alliance/8cdb78d2abbf0bb81a5d85d77eb815b096cf8577/media/alliance_template.png";
+            i.src = "https://raw.githubusercontent.com/AndMSWDE97/place_alliance/main/media/alliance_template.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
